50562aa887d08a6bdd80271341dbb12583a30a33
Mark Madden <mark.madden@spsmail.cuny.edu>
2014-09-02 21:48:53 -0400

    For week1 assignment, both files same except filename due to space used initially in filename causing problems.

---
+++ Week1.R
@@ -0,0 +1,3 @@
+# 1. I have R Version 3.1.1 (2014-07-10), and version 0.98.1028 of RStudio installed.
+# 2. I have version 9.3.5 of PostgreSQL.
+# 3. There are 401146 obs. of 5 variables of the data set 'sales' in the DMwR package.
