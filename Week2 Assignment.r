c4ddfc4c68fee8bb9e64083cc14e3a3fa51471bc
Mark Madden <mark.madden@spsmail.cuny.edu>
2014-09-09 23:16:27 -0400

    Create Week2 Assignment.r

---
+++ Week2 Assignment.r
@@ -0,0 +1,18 @@
+# Mark Madden Week 2 Assignment for IS360
+#
+# Question 1:
+queue <- c("James", "Mary", "Steve", "Alex", "Patricia")
+# Question 2:
+queue <- c(queue,"Harold")
+# Question 3:
+queue <- queue[2:6]
+# Question 4:
+queue <- c(queue[1:2],("Pam"),queue[3:5])
+# Question 5:
+queue <- queue[1:5]
+# Question 6:
+queue <- queue [ queue != "Alex"]
+# Question 7:
+which( queue == "Patricia")
+# Question 8:
+length(queue)
